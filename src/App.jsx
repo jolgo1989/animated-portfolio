f8fcc8401b76bc88ca9802758e535743be615f33
jolgo <jlgo60@hotmail.com>
2023-12-20 08:04:11 -0500

    Primer commir(efecto lama dev)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import "./App.scss"
+import Navbar from "./components/navbar/Navbar"
+import Test from "./Test"
+
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section><Navbar /></section>
+    <section>Perallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Porfolio1</section>
+    <section>Porfolio2</section>
+    <section>Contact</section>
+    {/* <Test />
+    <Test /> */}
+  </div>;
 };
 
 export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Navbar.scss"
+import { motion } from "framer-motion"
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            {/* sidebar */}
+            <div className="wrapper">
+                {/* Efecto logo lama Dev */}
+                <div className="social">
+                    <motion.span initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>Lama Dev</motion.span>
+                    <a href='#'><img src="/facebook.png" alt="" /></a>
+                    <a href='#'><img src="/instagram.png" alt="" /></a>
+                    <a href='#'><img src="/youtube.png" alt="" /></a>
+                    <a href='#'><img src="/dribbble.png" alt="" /></a>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
