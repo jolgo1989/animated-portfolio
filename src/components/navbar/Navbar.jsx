a95622f4c8ab17cc66ada4517d19b86b0bc09f67
jolgo <jlgo60@hotmail.com>
2023-12-21 09:49:33 -0500

    se agrego toggleButton

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
-import "./Navbar.scss"
-import { motion } from "framer-motion"
+import React from "react";
+import "./Navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-    return (
-        <div className='navbar'>
-            {/* sidebar */}
-            <div className="wrapper">
-                {/* Efecto logo lama Dev */}
-                <div className="social">
-                    <motion.span initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>Lama Dev</motion.span>
-                    <a href='#'><img src="/facebook.png" alt="" /></a>
-                    <a href='#'><img src="/instagram.png" alt="" /></a>
-                    <a href='#'><img src="/youtube.png" alt="" /></a>
-                    <a href='#'><img src="/dribbble.png" alt="" /></a>
-
-                </div>
-            </div>
+  return (
+    <div className="navbar">
+      {/* sidebar */}
+      <div className="wrapper">
+        {/* Efecto logo lama Dev */}
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Lama Dev
+        </motion.span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" alt="" />
+          </a>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
