d015a03c08dcb9f11c1b4d029e91f76f60fd54ab
jolgo <jlgo60@hotmail.com>
2023-12-22 05:02:58 -0500

    Se documento el sidebar

--- src/components/navbar/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/navbar/sidebar/toggleButton/ToggleButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const ToggleButton = ({ setOpen }) => {
+  // Maneja el clic del botón y cambia el estado del componente padre.
   return <button onClick={() => setOpen((prev) => !prev)}>Button</button>;
 };
 
--- src/components/navbar/sidebar/toggleButton/ToggleButton.scss
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ToggleButton = () => {
-  return <div>ToggleButton</div>;
-};
-
-export default ToggleButton;
