45a9d9e15f2a8c4f173c54ff3f8b940603e5fa85
jolgo <jlgo60@hotmail.com>
2023-12-23 05:12:22 -0500

    Se aplico animaciones a los links

--- src/components/navbar/sidebar/Sidebar.jsx
+++ src/components/navbar/sidebar/Sidebar.jsx
@@ -10,29 +10,29 @@ import { motion } from "framer-motion";
  * Este componente representa un sidebar con capacidad de animación al abrir y cerrar.
  *
  */
+// Variantes de animación para el componente motion.div
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 20,
+    },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
 const Sidebar = () => {
   // Estado para controlar si el sidebar está abierto o cerrado
   const [open, setOpen] = useState(false);
-
-  // Variantes de animación para el componente motion.div
-  const variants = {
-    open: {
-      clipPath: "circle(1200px at 50px 50px)",
-      transition: {
-        type: "spring",
-        stiffness: 20,
-      },
-    },
-    closed: {
-      clipPath: "circle(30px at 50px 50px)",
-      transition: {
-        delay: 0.5,
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-      },
-    },
-  };
 
   return (
     // Componente motion.div con animación basada en el estado 'open'
--- src/components/navbar/sidebar/links/Links.jsx
+++ src/components/navbar/sidebar/links/Links.jsx
@@ -1,16 +1,53 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+// Variantes de animación para el componente motion.div
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggereDirection: -1,
+    },
+  },
+};
+
+// Variantes de animación para el componente motion.a
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
 
 const Links = () => {
   const items = ["Homepage", "Services", "Porfolio", "Contact", "About"];
 
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          //Componentes de framer-motion
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
